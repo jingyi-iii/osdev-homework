baf6074c311859a5a0eb1bbf015df1656e27027d
Jingyio <linjingyi.gdut@foxmail.com>
2024-12-30 10:23:30 +0800

    refactor(barebones-mainline): initialize the GDT using C language

--- barebones-mainline/source/core/gdt.c
+++ barebones-mainline/source/core/gdt.c
@@ -1,6 +1,12 @@
-#include <stdint.h>
+#include "gdt.h"
 
-uint64_t gen_desc(uint32_t base, uint32_t limit, uint16_t flags)
+static uint64_t gdt[128] = { 0 };
+struct {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed)) gdtmeta = { 0 };
+
+uint64_t gen_gdesc(uint32_t base, uint32_t limit, uint16_t flags)
 {
     uint64_t desc = 0;
 
@@ -14,4 +20,24 @@ uint64_t gen_desc(uint32_t base, uint32_t limit, uint16_t flags)
     desc |= limit & 0x0000ffff;
 
     return desc;
+}
+
+uint64_t get_gsel(uint64_t* ptr_gdesc, uint8_t rpl)
+{
+    if (!ptr_gdesc || ptr_gdesc < gdt || rpl > 3)
+        return 0;
+
+    return ((ptr_gdesc - gdt) | rpl); 
+}
+
+void gdt_init(void)
+{
+    gdt[0] = gen_gdesc(0,       0,            0);
+    gdt[1] = gen_gdesc(0, 0xFFFFF, GDT_CODE_PL0);
+    gdt[2] = gen_gdesc(0, 0xFFFFF, GDT_DATA_PL0);
+
+    gdtmeta.limit = 128 * sizeof(uint64_t) - 1;
+    gdtmeta.base = (uint32_t)gdt;
+
+    __asm__("lgdt gdtmeta":::);
 }
