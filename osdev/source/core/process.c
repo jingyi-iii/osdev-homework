47b7714f433fd51c64c8de46326c73cea70be3dd
jingyi-iii <linjingyi.gdut@foxmail.com>
2026-04-19 14:23:39 +0800

    feat(osdev): daily update: 202604192223
    Signed-off-by: jingyi-iii <linjingyi.gdut@foxmail.com>

--- osdev/source/core/process.c
+++ osdev/source/core/process.c
@@ -6,6 +6,10 @@
 #include "module.h"
 #include "string.h"
 
+#define PROC_CTRL_SCHEDULE  (1)
+#define PROC_CTRL_BLOCK     (2)
+#define PROC_CTRL_UNBLOCK   (3)
+
 static pcb *proc_run = 0;
 
 int32_t create_proc(proc_priv priv, proc_entry_t entry)
@@ -24,6 +28,7 @@ int32_t create_proc(proc_priv priv, proc_entry_t entry)
         return -22;
     }
     proc->pid = pid++;
+    proc->state = PS_READY_TO_RUN;
 
     list_init(&proc->pcb_node);
     proc->sp_lock = spinlock_alloc();
@@ -58,7 +63,11 @@ static void isr_schedule(void* dev)
         return;
     timeslice = 0;
 
-    proc_run = list_entry(list_next(&proc_run->pcb_node), pcb, pcb_node);
+    while (1) {
+        proc_run = list_entry(list_next(&proc_run->pcb_node), pcb, pcb_node);
+        if (proc_run->state == PS_READY_TO_RUN)
+            break;
+    }
     spinlock_lock(proc_run->sp_lock);
     proc_restore_context(&proc_run->context);
     spinlock_unlock(proc_run->sp_lock);
@@ -66,16 +75,62 @@ static void isr_schedule(void* dev)
 
 static void syscall(void* data)
 {
-    (void)data;
-    proc_run = list_entry(list_next(&proc_run->pcb_node), pcb, pcb_node);
-    spinlock_lock(proc_run->sp_lock);
-    proc_restore_context(&proc_run->context);
-    spinlock_unlock(proc_run->sp_lock);
+    proc_ctrl_config *config = (proc_ctrl_config*)data;
+
+    switch (config->cmd) {
+    case PROC_CTRL_SCHEDULE:
+        proc_run = list_entry(list_next(&proc_run->pcb_node), pcb, pcb_node);
+        spinlock_lock(proc_run->sp_lock);
+        proc_restore_context(&proc_run->context);
+        spinlock_unlock(proc_run->sp_lock);
+        break;
+    case PROC_CTRL_BLOCK:
+        list_for_each(node, &proc_run->pcb_node) {
+            pcb* dev = list_entry(node, pcb, pcb_node);
+            if (dev->pid == config->pid) {
+                dev->state = PS_PENDING;
+                break;
+            }
+        }
+        break;
+    // case PROC_CTRL_UNBLOCK:
+    //     list_for_each(node, &proc_run->pcb_node) {
+    //         pcb* dev = list_entry(node, pcb, pcb_node);
+    //         if (dev->pid == config->pid) {
+    //             dev->state = PS_READY_TO_RUN;
+    //             break;
+    //         }
+    //     }
+    //     break;
+    default:
+        break;
+    }
 }
 
 void schedule(void)
 {
-    arch_syscall(0, 0);
+    proc_ctrl_config config = {0};
+    config.cmd = PROC_CTRL_SCHEDULE;
+
+    arch_syscall(0, &config);
+}
+
+void block(int32_t pid)
+{
+    proc_ctrl_config config = {0};
+    config.cmd = PROC_CTRL_BLOCK;
+    config.pid = pid;
+
+    arch_syscall(0, &config);  
+}
+
+void unblock(int32_t pid)
+{
+    proc_ctrl_config config = {0};
+    config.cmd = PROC_CTRL_UNBLOCK;
+    config.pid = pid;
+
+    arch_syscall(0, &config);  
 }
 
 static irqdev* pcb_irqdev = 0;
--- osdev/source/core/process.h
+++ osdev/source/core/process.h
@@ -12,11 +12,17 @@ typedef enum proc_run_state {
     PS_PENDING,
 } proc_state;
 
+typedef struct proc_ctrl_config {
+    uint8_t cmd;
+    int32_t pid;
+} proc_ctrl_config;
+
 /* Process Control Block */
 typedef struct pcb {
     proc_context    context;
     proc_entry_t    entry;
     int32_t         pid;
+    proc_state      state;
     list_node       pcb_node;
     spinlock*       sp_lock;
 } pcb;
@@ -25,5 +31,7 @@ typedef void (*proc_entry_t)(void);
 
 int32_t create_proc(proc_priv priv, proc_entry_t entry);
 void schedule(void);
+void block(int32_t pid);
+void unblock(int32_t pid);
 
 #endif
